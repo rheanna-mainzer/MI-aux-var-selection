aed14baf2254205c34f9e8c490df2b323f3d8e12
Rheanna Mainzer <61679636+rheanna-mainzer@users.noreply.github.com>
2021-12-09 14:27:32 +1100

    Upload functions for simulation study

---
+++ an_simdata.R
@@ -0,0 +1,374 @@
+# Analyse simulated data
+# an_simdata
+#
+# This function will apply the analysis strategies in strategy_vec to one batch 
+# of simulated datasets (250 per batch) and output the desired estimates
+# 
+# Input:
+# scenario: "basic", "extreme" or "realistic"
+# N: batch number (1 - 8)
+# strategy_vec: vector of analysis strategies (strings):
+#   "cc": complete case analysis
+#   "mice-full": all auxiliary variables included in the imputation model
+#   "quickpred-pt2": mice quickpred strategy with mincor = 0.2
+#   "quickpred-pt4": mice quickpred strategy with mincor = 0.4
+#   "pcaux": principal components as auxiliary variables
+#   "forward-fmi": forward selection based on the FMI
+#   "tests": auxiliary variables selection using t tests
+#   "LASSO": auxiliary variables selection using the LASSO
+# m: number of imputations
+# maxit: number of iterations for mice (not important for univariate missingness)
+#
+# Functions:
+# pred_ttest.R: select auxiliary variables using t tests
+# calc_FMI.R: calculate FMI
+# select_aux_forwardFMI_simstudy.R: forward selection of auxiliary variables using FMI
+# 
+# Output:
+# Data frame containing the following columns:
+#   1. scenario: scenario name
+#   2. nsim: simulated dataset number
+#   3. straetgy: analysis strategy
+#   4. bx: estimate of the regression coefficient (y ~ x)
+#   5. bx_se: standard error of bx
+#   6. bx_ll: lower limit of 95% confidence interval for beta x
+#   7. bx_ul: upper limit of 95% confidence interval for beta x
+#   8. ybar: estimate of mean of y
+#   9. ybar_se: standard error of ybar
+#   10. ybar_ll: lower limit of the 95% confidence interval for mean of y
+#   11. ybar_ul: upper limit of the 95% confidence interval for mean of y
+#   12. imp_vars: variables selected for the imputation model
+#
+# Notes: 
+# Forward and Forward-sw are implemented in separate functions in Stata
+#
+# Written by R Mainzer, Jan 2021
+
+an_simdata <- function(scenario, N, strategy_vec, m, maxit){
+  
+  # Set up data frame to store results
+  n_strat <- length(strategy_vec)
+  n_row <- 250 * n_strat
+  res <- data.frame(scenario = scenario,
+                    nsim = NA,
+                    strategy = rep("NA", n_row), 
+                    bx = NA, 
+                    bx_se = NA, 
+                    bx_ll = NA, 
+                    bx_ul = NA,
+                    ybar = NA, 
+                    ybar_se = NA, 
+                    ybar_ll = NA, 
+                    ybar_ul = NA,
+                    imp_vars = "NA")
+  
+  # Row for updating results
+  r <- 1 
+  
+  # Run analysis in 8 batches of 250 each (2000 total)
+  for(k in ((N - 1) * 250 + 1):(N * 250)){
+    
+    # Read in data set
+    dat <- read.table(paste("data/simdat_", scenario, "_", paste(sprintf("%04d", k)), ".csv", sep = ""))
+    
+    # Sample size
+    n <- nrow(dat) 
+    
+    # Loop over strategies
+    for(j in 1:n_strat){
+      
+      strategy <- strategy_vec[j]
+      
+      # Complete case analysis
+      if(strategy == "cc"){
+        
+        # No imputation model
+        imp_vars <- NA
+        
+        # Estimate of beta_x
+        mod1 <- lm(y ~ x, data = dat)
+        summ_mod1 <- summary(mod1)
+        bx <- summ_mod1$coefficients[2, "Estimate"]
+        bx_se <- summ_mod1$coefficients[2, "Std. Error"]
+        bx_ll <- confint(mod1)[2, "2.5 %"]
+        bx_ul <- confint(mod1)[2, "97.5 %"]
+        
+        # Estimate of mu_Y
+        mod2 <- lm(y ~ 1, data = dat)
+        summ_mod2 <- summary(mod2)
+        ybar <- summ_mod2$coefficients[1, "Estimate"]
+        ybar_se <- summ_mod2$coefficients[1, "Std. Error"]
+        ybar_ll <- confint(mod2)[1, "2.5 %"]
+        ybar_ul <- confint(mod2)[1, "97.5 %"]
+        
+      }
+      
+      # Full imputation model using mice
+      if(strategy == "mice-full"){
+        
+        # Do MI
+        imp_dat <- dat
+        imp <- mice(dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(which(imp$predictorMatrix["y",] == 1))
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # Auxiliary variable selection using quickpred with mincor = 0.2
+      if(strategy == "quickpred-pt2"){
+        
+        # Do MI
+        imp_dat <- dat
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    pred = quickpred(dat, mincor = 0.2, minpuc = 0, include = "x"),
+                    print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(which(imp$predictorMatrix["y",] == 1))
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # Auxiliary variable selection using quickpred with mincor = 0.4
+      if(strategy == "quickpred-pt4"){
+        
+        # Do MI
+        imp_dat <- dat
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    pred = quickpred(dat, mincor = 0.4, minpuc = 0, include = "x"),
+                    print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(which(imp$predictorMatrix["y",] == 1))
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # Principal components of auxiliary variables
+      if(strategy == "pcaux"){
+        
+        # Prepare the data
+        prep_dat <- cbind(id = 1:n, dat)
+        aux_dat <- select(prep_dat, c("id", starts_with("a")))
+        prep_aux_dat <- prepData(rawData = aux_dat, idVars = "id", verbose = 0)
+        
+        # Extract principal component scores
+        pc_aux_dat <- createPcAux(pcAuxData = prep_aux_dat, nComps = c(0.4, 0), doImputation = FALSE,
+                                  verbose = 0)
+        all_dat <- mergePcAux(pcAuxData = pc_aux_dat, rawData = prep_dat)
+        imp_dat <- select(all_dat, c("y", "x", starts_with("lin")))
+        
+        # Do MI
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(imp$predictorMatrix["y",])
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # Forward selection based on fraction of missing information
+      if(strategy == "forward-fmi"){
+        
+        # Prepare the data
+        aux_dat <- select(dat, starts_with("a"))
+        aux_dat_selected <- select_aux_forwardFMI_simstudy(dat$y, aux_dat, scenario)
+        imp_dat <- cbind(y = dat$y, x = dat$x, aux_dat_selected$A_set)
+        
+        # Do MI
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(imp$predictorMatrix["y",])
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # Auxiliary variable selection using t tests
+      if(strategy == "tests"){
+        
+        # Auxiliary variable selection
+        my <- ifelse(is.na(dat$y), 1, 0)
+        aux_dat <- select(dat, starts_with("a"))
+        row1_pred <- sapply(aux_dat, FUN = pred_ttest, group = my, simplify = "array")
+        
+        # Make predictor matrix
+        imp_dat <- dat
+        pred <- make.predictorMatrix(imp_dat)
+        pred["y", names(row1_pred)] <- row1_pred
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    predictorMatrix = pred, print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(which(imp$predictorMatrix["y", ] == 1))
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      # LASSO
+      if(strategy == "LASSO"){
+        
+        # LASSO requires complete predictors in matrix form
+        complete_dat <- dat[!is.na(dat$y), ]
+        y <- complete_dat$y
+        X <- as.matrix(complete_dat[, 2:ncol(dat)])
+        
+        # Choose model using cross validation
+        cvfit <- cv.glmnet(X, y)
+
+        # Create the row of the predictor matrix used to impute y
+        pred_dat <- data.frame(var = coef(cvfit)@Dimnames[[1]], coef = matrix(coef(cvfit, s = "lambda.1se")))
+        pred_dat <- pred_dat[-1,]
+        
+        # First entry is 0 as y is not used on the RHS of the imputation model for y
+        pred_row <- c(0, ifelse(pred_dat$coef != 0, 1, 0))
+        
+        # Get predictor matrix and update
+        imp_dat <- dat
+        imp <- mice(imp_dat, maxit = 0, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    print = FALSE)
+        pred <- imp$predictorMatrix
+        pred[1, ] <- pred_row
+        
+        # Do MI
+        imp <- mice(imp_dat, m = m, maxit = maxit, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
+                    predictorMatrix = pred, print = FALSE)
+        
+        # Variables included in the imputation model for y
+        imp_vars <- names(which(imp$predictorMatrix["y",] == 1))
+        
+        # Estimate of beta_x
+        fit1 <- with(imp, lm(y ~ x))
+        pool1 <- summary(pool(fit1), conf.int = TRUE)
+        bx <- pool1[2, "estimate"]
+        bx_se <- pool1[2, "std.error"]
+        bx_ll <- pool1[2, "2.5 %"]
+        bx_ul <- pool1[2, "97.5 %"]
+        
+        # Estimate of mu_y
+        fit2 <- with(imp, lm(y ~ 1))
+        pool2 <- summary(pool(fit2), conf.int = TRUE)
+        ybar <- pool2[1, "estimate"]
+        ybar_se <- pool2[1, "std.error"]
+        ybar_ll <- pool2[1, "2.5 %"]
+        ybar_ul <- pool2[1, "97.5 %"]
+        
+      }
+      
+      res[r, 2] <- k
+      res[r, 3] <- strategy
+      res[r, 4] <- bx
+      res[r, 5] <- bx_se
+      res[r, 6] <- bx_ll
+      res[r, 7] <- bx_ul
+      res[r, 8] <- ybar
+      res[r, 9] <- ybar_se
+      res[r, 10] <- ybar_ll
+      res[r, 11] <- ybar_ul
+      res[r, 12] <- paste(imp_vars, collapse = ' ')
+      
+      r <- r + 1
+
+    }
+    
+  }
+
+  # Return results
+  out <- res
+  
+}
---
+++ prop_aux_selected.R
@@ -0,0 +1,159 @@
+add_a <- function(x){
+  # This function will add "a" to the start of each element in a vector
+  
+  for(i in 1:length(x)){
+    x[i] <- paste("a", x[i], sep = "")
+  }
+  x
+}
+
+grp_assign <- function(x, DGP){
+  # This function will group auxiliary variables
+  
+  if(length(which(DGP$group == x)) != 0){
+    out <- add_a(which(DGP$group == x))
+  } else {
+    out <- NA
+  }
+  out
+}
+
+len_grps <- function(grp){
+  # This function will return the number of auxiliary variables in a group
+  
+  if(anyNA(grp)){
+    out <- 0
+  } else {
+    out <- length(grp)
+  }
+  out
+}
+
+summ_aux <- function(grpno, grp_lens, strategy, res2){
+  # This function will return the average number and percentage (across simulations) 
+  # of auxiliary variables in a group, for a given strategy
+  
+  if(grp_lens[grpno] != 0){
+    a <- mean(res2[which(res2$strategy == strategy), grpno + 2])
+    b <- a / grp_lens[grpno] * 100
+    out <- paste0(round(a, 1), " (", round(b, 1), ")")
+  } else {
+    out <- paste0("-")
+  }
+  out
+}
+
+prop_aux_selected <- function(res, cor_a, gamma_a){
+  
+  # This function will calculate the average number (and proportion) of 
+  # auxiliary variables in each of eights groups (see below details) that were 
+  # selected by the analysis strategy, as well as the average number 
+  # (proportion) in total
+  #
+  # Input:
+  # res: data frame containing simulation study results for a given scenario
+  # cor_a: vector of correlations between a and y used in DGP
+  # gamma_a: vector of regression coefficients used in DGP
+  #
+  # Details: 
+  # The groups are defined as follows:
+  # grp1: 0 <= |correlation| < 0.1 and associated with missingness
+  # grp2: 0 <= |correlation| < 0.1 and not associated with missingness
+  # grp3: 0.1 <= |correlation| < 0.1 and associated with missingness
+  # grp4: 0.1 <= |correlation| < 0.1 and not associated with missingness
+  # grp5: 0.2 <= |correlation| < 0.1 and associated with missingness
+  # grp6: 0.2 <= |correlation| < 0.1 and not associated with missingness
+  # grp7: |correlation| >= 0.4 and associated with missingness
+  # grp8: |correlation| >= 0.4 and not associated with missingness
+  #
+  # Written by R Mainzer, August 2021
+  
+  # Parameters used to define groups
+  p <- length(cor_a)
+  DGP <- data.frame(a = 1:p, cor_a = cor_a, gamma_a = gamma_a)
+  
+  # Filter to extract variable selection strategies
+  res <- rbind(filter(res, str_detect(strategy, "quickpred-pt2")),
+               filter(res, str_detect(strategy, "quickpred-pt4")),
+               filter(res, str_detect(strategy, "forward")),
+               filter(res, str_detect(strategy, "tests")),
+               filter(res, str_detect(strategy, "LASSO")))
+  
+  # Group auxiliary variables using known correlation and association with missingness
+  DGP$group <- ifelse(0 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.1 & DGP$gamma_a != 0, 1, 0)
+  DGP$group <- ifelse(0 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.1 & DGP$gamma_a == 0, 2, DGP$group)
+  DGP$group <- ifelse(0.1 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.2 & DGP$gamma_a != 0, 3, DGP$group)
+  DGP$group <- ifelse(0.1 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.2 & DGP$gamma_a == 0, 4, DGP$group)
+  DGP$group <- ifelse(0.2 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.4 & DGP$gamma_a != 0, 5, DGP$group)
+  DGP$group <- ifelse(0.2 <= abs(DGP$cor_a) & abs(DGP$cor_a) < 0.4 & DGP$gamma_a == 0, 6, DGP$group)
+  DGP$group <- ifelse(abs(DGP$cor_a) >= 0.4 & DGP$gamma_a != 0, 7, DGP$group)
+  DGP$group <- ifelse(abs(DGP$cor_a) >= 0.4 & DGP$gamma_a == 0, 8, DGP$group)
+
+  # Variables in each group
+  grp1 <- grp_assign(1, DGP)
+  grp2 <- grp_assign(2, DGP)
+  grp3 <- grp_assign(3, DGP)
+  grp4 <- grp_assign(4, DGP)
+  grp5 <- grp_assign(5, DGP)
+  grp6 <- grp_assign(6, DGP)
+  grp7 <- grp_assign(7, DGP)
+  grp8 <- grp_assign(8, DGP)
+
+  # Vector of group lengths
+  grp_lens <- c(len_grps(grp1), len_grps(grp2), len_grps(grp3), len_grps(grp4), 
+                len_grps(grp5), len_grps(grp6), len_grps(grp7), len_grps(grp8))
+  grp_lens <- c(grp_lens, sum(grp_lens))
+
+  # Set up data frame to store results  
+  res2 <- data.frame(strategy = "NA", nsim = NA, 
+                    grp1 = NA, grp2 = NA, grp3 = NA, grp4 = NA, 
+                    grp5 = NA, grp6 = NA, grp7 = NA, grp8 = NA,
+                    total = NA)
+  
+  # Count how many selected auxiliary variables were in each group for each 
+  # dataset and analysis strategy
+  for(i in 1:nrow(res)){
+    
+    x <- unlist(str_split(res$imp_vars[i], pattern = " "))
+    grp1_len <- length(intersect(x, grp1))
+    grp2_len <- length(intersect(x, grp2))
+    grp3_len <- length(intersect(x, grp3))
+    grp4_len <- length(intersect(x, grp4))
+    grp5_len <- length(intersect(x, grp5))
+    grp6_len <- length(intersect(x, grp6))
+    grp7_len <- length(intersect(x, grp7))
+    grp8_len <- length(intersect(x, grp8))
+    total <- sum(grp1_len, grp2_len, grp3_len, grp4_len, 
+                 grp5_len, grp6_len, grp7_len, grp8_len) 
+    
+    tmp <- data.frame(strategy = res$strategy[i], nsim = res$nsim[i],
+                      grp1 = grp1_len, grp2 = grp2_len, grp3 = grp3_len, 
+                      grp4 = grp4_len, grp5 = grp5_len, grp6 = grp6_len, 
+                      grp7 = grp7_len, grp8 = grp8_len, total = total)
+    res2 <- rbind(res2, tmp)
+    
+  }
+  
+  # Remove first row of NAs
+  res2 <- res2[-1, ]
+  
+  # Calculate average number and percentage (across simulations) of auxiliary 
+  # variables in a group for each analysis strategy
+  out <- as.data.frame(
+        rbind(unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "quickpred-pt2", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "quickpred-pt4", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "forward", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "forward-sw", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "forward-fmi", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "tests", res2 = res2)),
+              unlist(lapply(1:9, summ_aux, grp_lens = grp_lens, strategy = "LASSO", res2 = res2))))
+  
+  # Update row and column names
+  colnames(out) <- c("grp1", "grp2", "grp3", "grp4", "grp5", "grp6", "grp7", "grp8", "total")
+  out <- cbind(strategy = c("quickpred-pt2", "quickpred-pt4", "forward", "forward-sw", "forward-fmi", "tests", "LASSO"),
+                         out)
+  
+  # Output results
+  list(out, grp_lens)
+  
+}
